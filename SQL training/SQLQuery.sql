4a8db2d335c393a18bb7960d57f03cc462279bc1
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-06-27 09:33:52 +0300

    Add 29 problems with their answers using sql

--- SQL training/SQLQuery.sql
+++ SQL training/SQLQuery.sql
@@ -193,3 +193,237 @@ where NumDoors is null;
 -- Problem 21: Get Total Vehicles that number of doors is not specified
 select COUNT(*) as TotalVehiclesWithoutDoors from VehicleDetails
 where NumDoors is null;
+
+
+-- Problem 22: Get percentage of vehicles that has no doors specified
+select 
+(
+	cast((select COUNT(*) as TotalVehiclesWithoutDoors from VehicleDetails where NumDoors is null) as float) / cast((select COUNT(*) as TotalVechicles from VehicleDetails) as float)
+) as Per;
+
+
+-- Problem 23: Get MakeID , Make, SubModelName for all vehicles that have SubModelName 'Elite'
+SELECT       VehicleDetails.MakeID, Makes.Make, SubModels.SubModelName
+FROM            VehicleDetails INNER JOIN
+                         Makes ON VehicleDetails.MakeID = Makes.MakeID INNER JOIN
+                         SubModels ON VehicleDetails.SubModelID = SubModels.SubModelID
+WHERE		SubModels.SubModelName = N'Elite';
+
+
+-- Problem 24: Get all vehicles that have Engines > 3 Liters and have only 2 doors
+select * from VehicleDetails
+where Engine_Liter_Display > 3 and NumDoors = 2;
+
+
+-- Problem 25: Get make and vehicles that the engine contains 'OHV' and have Cylinders = 4
+SELECT       Makes.Make, VehicleDetails.*
+FROM            VehicleDetails INNER JOIN
+                         Makes ON VehicleDetails.MakeID = Makes.MakeID
+WHERE		Engine like '%OHV%' and Engine_Cylinders = 4;
+
+
+-- Problem 26: Get all vehicles that their body is 'Sport Utility' and Year > 2020
+SELECT       Bodies.BodyName, VehicleDetails.*
+FROM            VehicleDetails INNER JOIN
+                         Bodies ON VehicleDetails.BodyID = Bodies.BodyID
+WHERE		Bodies.BodyName = N'Sport Utility' and VehicleDetails.Year > 2020;
+
+
+-- Problem 27: Get all vehicles that their Body is 'Coupe' or 'Hatchback' or 'Sedan'
+SELECT       Bodies.BodyName, VehicleDetails.*
+FROM            VehicleDetails INNER JOIN
+                         Bodies ON VehicleDetails.BodyID = Bodies.BodyID
+WHERE		 Bodies.BodyName in ('Coupe', 'Hatchback', 'Sedan');
+
+
+-- Problem 28: Get all vehicles that their body is 'Coupe' or 'Hatchback' or 'Sedan' and manufactured in year 2008 or 2020 or 2021
+SELECT       Bodies.BodyName, VehicleDetails.*
+FROM            VehicleDetails INNER JOIN
+                         Bodies ON VehicleDetails.BodyID = Bodies.BodyID
+WHERE		 Bodies.BodyName in ('Coupe', 'Hatchback', 'Sedan') and VehicleDetails.Year in (2008, 2020, 2021);
+
+
+-- Problem 29: Return found=1 if there is any vehicle made in year 1950
+select found = 1
+where exists (select top 1 * from VehicleDetails where year = 1980);
+
+
+-- Problem 30: Get all Vehicle_Display_Name, NumDoors and add extra column to describe number of doors by words, and if door is null display 'Not Set'
+select Vehicle_Display_Name, NumDoors,
+case
+	when NumDoors = 0 then 'zero doors'
+	when NumDoors = 1 then 'one doors'
+	when NumDoors = 2 then 'two doors'
+	when NumDoors = 3 then 'three doors'
+	when NumDoors = 4 then 'four doors'
+	when NumDoors = 5 then 'five doors'
+	when NumDoors = 6 then 'six doors'
+	when NumDoors = 8 then 'eight doors'
+	when NumDoors is null then 'not set'
+	else 'unknown'
+end as DoorDescription
+from VehicleDetails;
+
+
+-- Problem 31: Get all Vehicle_Display_Name, year and add extra column to calculate the age of the car then sort the results by age desc.
+select Vehicle_Display_Name, Year, Age = YEAR(GetDate()) - Year from VehicleDetails
+order by age desc;
+
+
+-- Problem 32: Get all Vehicle_Display_Name, year, Age for vehicles that their age between 15 and 25 years old
+select * from 
+(
+	select Vehicle_Display_Name, Year, Age = YEAR(GetDate()) - Year from VehicleDetails
+) newTable
+where age between 15 and 25;
+
+
+-- Problem 33: Get Minimum Engine CC , Maximum Engine CC , and Average Engine CC of all Vehicles
+select * from VehicleDetails;
+select MIN(Engine_CC) as MinCC, AVG(Engine_CC) as AvgCC, MAX(Engine_CC) as MaxCC from VehicleDetails;
+
+
+-- Problem 34: Get all vehicles that have the minimum Engine_CC
+select * from VehicleDetails
+where Engine_CC = (select MIN(Engine_CC) from VehicleDetails);
+
+
+-- Problem 35: Get all vehicles that have the Maximum Engine_CC
+select * from VehicleDetails
+where Engine_CC = (select MAX(Engine_CC) from VehicleDetails);
+
+
+-- Problem 36: Get all vehicles that have Engin_CC below average
+select * from VehicleDetails
+where Engine_CC < (select AVG(Engine_CC) from VehicleDetails);
+
+
+-- Problem 37: Get total vehicles that have Engin_CC above average
+select COUNT(*) as TotalVehiclesAboveTheCCAvg from VehicleDetails
+where Engine_CC > (select AVG(Engine_CC) from VehicleDetails);
+
+
+-- Problem 38: Get all unique Engin_CC and sort them Desc
+select distinct Engine_CC from VehicleDetails
+order by Engine_CC desc;
+
+
+-- Problem 39: Get the maximum 3 Engine CC
+select distinct top 3 Engine_CC from VehicleDetails
+order by Engine_CC desc;
+
+
+-- Problem 40: Get all vehicles that has one of the Max 3 Engine CC
+select * from VehicleDetails
+where Engine_CC in 
+(
+	select distinct top 3 Engine_CC from VehicleDetails
+	order by Engine_CC desc
+);
+
+
+-- Problem 41: Get all Makes that manufactures one of the Max 3 Engine CC
+SELECT       Makes.Make, VehicleDetails.Engine_CC
+FROM            VehicleDetails INNER JOIN
+                         Makes ON VehicleDetails.MakeID = Makes.MakeID
+WHERE		 Engine_CC in 
+			(
+				select distinct top 3 Engine_CC from VehicleDetails
+				order by Engine_CC desc
+			)
+ORDER BY make;
+
+
+-- Problem 42: Get a table of unique Engine_CC and calculate tax per Engine CC
+select Engine_CC,
+case
+	when Engine_CC between 0 and 1000 then 100
+	when Engine_CC between 1001 and 2000 then 200
+	when Engine_CC between 2001 and 4000 then 300
+	when Engine_CC between 4001 and 6000 then 400
+	when Engine_CC between 6001 and 8000 then 500
+	when Engine_CC > 8000 then 600
+	else 0
+end Tax
+from (select distinct Engine_CC from VehicleDetails) newTable
+order by Engine_CC;
+
+
+-- Problem 43: Get Make and Total Number Of Doors Manufactured Per Make
+SELECT       Makes.Make, SUM(NumDoors) as TotalDoors
+FROM            VehicleDetails INNER JOIN
+                         Makes ON VehicleDetails.MakeID = Makes.MakeID
+group by Makes.Make;
+
+
+-- Problem 44: Get Total Number Of Doors Manufactured by 'Ford'
+SELECT       Makes.Make, SUM(NumDoors) as TotalDoors
+FROM            VehicleDetails INNER JOIN
+                         Makes ON VehicleDetails.MakeID = Makes.MakeID
+group by Makes.Make
+having Makes.Make = 'Ford';
+
+
+-- Problem 45: Get Number of Models Per Make
+SELECT       Makes.Make, COUNT(*) as NumberOfModels
+FROM            Makes INNER JOIN
+                         MakeModels ON Makes.MakeID = MakeModels.MakeID
+group by Makes.Make
+order by NumberOfModels desc;
+
+
+-- Problem 46: Get the highest 3 manufacturers that make the highest number of models
+SELECT       top 3 Makes.Make, COUNT(*) as NumberOfModels
+FROM            Makes INNER JOIN
+                         MakeModels ON Makes.MakeID = MakeModels.MakeID
+group by Makes.Make
+order by NumberOfModels desc;
+
+
+-- Problem 47: Get the highest number of models manufactured
+select MAX(NumberOfModels) as HighestNumberOfModels from
+(
+	SELECT       Makes.Make, COUNT(*) as NumberOfModels
+	FROM            Makes INNER JOIN
+							MakeModels ON Makes.MakeID = MakeModels.MakeID
+	group by Makes.Make
+) newTable;
+
+
+-- Problem 48: Get the highest Manufacturers manufactured the highest number of models
+SELECT       Makes.Make, COUNT(*) as NumberOfModels
+FROM            Makes INNER JOIN
+						MakeModels ON Makes.MakeID = MakeModels.MakeID
+group by Makes.Make
+having COUNT(*) =
+(
+	select MAX(NumberOfModels) as HighestNumberOfModels from
+	(
+		SELECT       Makes.Make, COUNT(*) as NumberOfModels
+		FROM            Makes INNER JOIN
+								MakeModels ON Makes.MakeID = MakeModels.MakeID
+		group by Makes.Make
+	) newTable
+);
+
+
+-- Problem 49: Get the Lowest Manufacturers manufactured the lowest number of models
+SELECT       Makes.Make, COUNT(*) as NumberOfModels
+FROM            Makes INNER JOIN
+						MakeModels ON Makes.MakeID = MakeModels.MakeID
+group by Makes.Make
+having COUNT(*) = 
+(
+	select MIN(NumberOfModels) as LowestNumberOfModels from
+	(
+		SELECT       Makes.Make, COUNT(*) as NumberOfModels
+		FROM            Makes INNER JOIN
+								MakeModels ON Makes.MakeID = MakeModels.MakeID
+		group by Makes.Make
+	) newTable
+);
+
+
+-- Problem 50: Get all Fuel Types , each time the result should be showed in random order
+select * from FuelTypes
+order by NewID(); -- =The NewID() function will generate GUID for each row
